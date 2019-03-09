b6c88dafe56fa2083a449d8dba1a92e5e67d6565
lr53 <46905748+lr53@users.noreply.github.com>
2019-03-09 17:23:58 +0000

    Update Staff.java

--- Coursework/src/staff/Staff.java
+++ Coursework/src/staff/Staff.java
@@ -1,5 +1,9 @@
 package staff;
 
+
+import shop.Basket;
+import shop.Queue;
+
 /**
  * This is the staff class
  * 
@@ -9,19 +13,71 @@ package staff;
  */
 
 
-public class Staff {
+public class Staff implements Runnable {
 	
 	private int StaffID;
 	private String firstName;
 	private String lastName;
+
+	private Queue queue;
+	private Basket unprocessedOrder;
+	private static final int orderTime = 5000; //milliseconds
 	// could possibly add wage, no. of sales etc.
 	
 	
-	public Staff(int staffID, String firstname, String lastname) {
-		this.StaffID  = staffID;
+	public Staff(int StaffID, String firstname, String lastname) {
+		this.StaffID  = StaffID;
 		this.firstName = firstname;
 		this.lastName = lastname;				
 	}
+	
+	public Staff(int StaffID, String firstname, String lastname, Queue queue) {
+		this.StaffID  = StaffID;
+		this.firstName = firstname;
+		this.lastName = lastname;
+		this.queue = queue;
+	}
+	
+	public void run() {
+		
+		while(!queue.queueEmpty()) {
+			try {
+				 System.out.println ("Thread " + Thread.currentThread().getId() + " is running");
+				processOrder();
+				unprocessedOrder.confirmedAndPaid();
+				updateLog2();
+				
+			} catch (InterruptedException e) {
+				
+			}
+		}
+	
+	}
+	
+	private void processOrder() throws InterruptedException {
+		unprocessedOrder = queue.getNextInQueue();
+		updateLog();
+		Thread.sleep(orderTime);
+	}
+	
+	
+	public String updateLog() {
+		System.out.println("Customer ID: " + unprocessedOrder.getCurrentCustomerID() +  " order is being processed by Server " + StaffID +
+				"\n"+ "This order contains " + unprocessedOrder.getItemsInBasket());
+		String order = new String();
+		order += "Customer ID: " + unprocessedOrder.getCurrentCustomerID() +  " order is being processed\n"; 
+		order += "This order contains items" + unprocessedOrder.getItemsInBasket() + "\n";
+		return order;
+	}
+	
+	
+	public String updateLog2() {
+		System.out.println("Customer ID: " + unprocessedOrder.getCurrentCustomerID() +  " has been processed");
+		String order = new String();
+		order += "orderID: " + "has been processed";
+		return order;
+	}
+	
 	
 	
 	public int getStaffID() {
